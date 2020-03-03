28c11ff158c78e49609a26c7448c617425b61e19
Kristoffer Langeland Knudsen <kristofferlknudsen@gradientsoftware.net>
2020-03-03 15:45:28 +0100

    Made a few changes to get it to run. Changed returned image from double -> single; AutoScale only works on single (float) images, and will otherwise ignore the returned image.

--- gadgetron4.1_functions/buffer_recon_SPEN.m
+++ gadgetron4.1_functions/buffer_recon_SPEN.m
@@ -2,10 +2,8 @@ function buffer_recon_SPEN(connection) % 'Buffer' is a colloquial name for Recon
 disp("Matlab SPEN buffer reconstruction running.")
 
 %% Add to path location of reconstruct_SPEN function
-addpath('/home/mygadg/Documents/MATLAB/SPEN_Gadgetron_4');
 Parameters = struct;
 
-%   connection.next(); % Discard the first acquisition - it's noise data.
 try
     while true
         next_acquisition = @connection.next;
